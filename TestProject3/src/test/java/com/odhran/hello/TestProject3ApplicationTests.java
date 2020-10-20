722b56f93a3f3f25d08314bd00163cba80f6f562
Odhran Roche <odhranroche@gmail.com>
2020-10-20 16:10:17 +0300

    initial

---
+++ TestProject3/src/test/java/com/odhran/hello/TestProject3ApplicationTests.java
@@ -0,0 +1,30 @@
+package com.odhran.hello;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TestProject3ApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void getGreeting() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/greeting?name=odhran").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hello, odhran!")));
+
+	}
+}
